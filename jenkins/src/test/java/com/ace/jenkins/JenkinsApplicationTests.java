6d703c1988e9374797e9791c25500af6777e81e4
bohlale <ohl.badr0@gmail.com>
2024-01-18 18:25:17 +0100

    test

---
+++ jenkins/src/test/java/com/ace/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ace.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
